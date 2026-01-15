368f69ee020027d62117e5a73d355ea83ec7627a
jomacrist0 <joao.cristo@fiap.com.br>
2026-01-15 10:11:55 -0300

    🔄 Pagina_inicial redirects to Saldos

---
+++ Pagina_inicial.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+# Redirecionar automaticamente para a página de Saldos
+st.set_page_config(page_title="💰 Saldos do Ecossistema", page_icon="💰", layout="wide")
+
+# Switch para a página de Saldos
+st.switch_page("pages/1Saldos_do_Ecossistema.py")
